9d59e61876d5b805100c73ec28d26e23d3f76837
ales231 <alalbanto@uide.edu.ec>
2026-03-18 14:00:19 +0000

    commit2

--- practical_workshop_1.sh
+++ practical_workshop_1.sh
@@ -1,3 +1,8 @@
 uname -a
 which gpg
 gpg --version
+gpg --full-generate-key
+gpg --list-keys
+gpg --armor --export
+gpg --armor --export alexfalbant@gmail.com> mi_llave_publica.asc
+cat mi_llave_publica.asc
