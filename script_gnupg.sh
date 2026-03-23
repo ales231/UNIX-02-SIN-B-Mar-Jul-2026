64c9b2f9f84fe65d171984ccc2833285bb333f88
ales231 <alalbanto@uide.edu.ec>
2026-03-23 12:44:47 +0000

    commit23/23

--- script_gnupg.sh
+++ script_gnupg.sh
@@ -1,2 +1,3 @@
 gpg --list-secret-keys -keyid-format=long  #para listar las llaves
 gpg --armor --export-secret-keys 2BD74A3FCDD98B69 #con nuestra firma ejecutamos este comando y exporta tu llave privada
+gpg --list-keys #antes de hacer publica vamos a listar las llaves
