a364ee52f672a79ad1c7741a8c4d2aaf613c03b4
ales231 <alalbanto@uide.edu.ec>
2026-03-23 13:13:49 +0000

    commit56

--- script_gnupg.sh
+++ script_gnupg.sh
@@ -2,3 +2,4 @@ gpg --list-secret-keys -keyid-format=long  #para listar las llaves
 gpg --armor --export-secret-keys 2BD74A3FCDD98B69 #con nuestra firma ejecutamos este comando y exporta tu llave privada
 gpg --list-keys #antes de hacer publica vamos a listar las llaves
 gpg --import MICOMPA_llave_publica.asc #con esto exportamos la llave de my friend
+gpg --output doc_no_cifrado.txt --encrypt --recipient 573E1A32AF37CBFA47B03919B84A19B106C03AD7 doc_no_cifrado.txt #usamos el hash de la clave publica que importo de su compa para cifrar el documento
