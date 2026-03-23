d8780c6bbd529198624c989b941188ed0e1c6584
ales231 <alalbanto@uide.edu.ec>
2026-03-23 12:58:54 +0000

    commit2026

--- script_gnupg.sh
+++ script_gnupg.sh
@@ -1,3 +1,4 @@
 gpg --list-secret-keys -keyid-format=long  #para listar las llaves
 gpg --armor --export-secret-keys 2BD74A3FCDD98B69 #con nuestra firma ejecutamos este comando y exporta tu llave privada
 gpg --list-keys #antes de hacer publica vamos a listar las llaves
+gpg --import MICOMPA_llave_publica.asc #con esto exportamos la llave de my friend
