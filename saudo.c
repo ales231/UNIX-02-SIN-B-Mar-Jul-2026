fe303c9ec62f21289ede97deffc1a6ffbebc2c3c
ales231 <alalbanto@uide.edu.ec>
2026-03-25 13:04:54 +0000

    commit arsenal

--- saudo.c
+++ saudo.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
-int main() {
-    printf("Hello World!\n");
-    return 0;
+#include <stdio.h> //incluye la librería estándar de entrada y salida de C, gracias a esto podemos usar el printf
+int main() {  //aqui empieza la funcion principal
+    printf("Hello World!\n"); //muestra en la pantalla
+    return 0;  //indica que el programa termino correctamente
 }
