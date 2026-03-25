9bf01ad91fdc02e06e464b23fcbce2c1bc98d511
ales231 <alalbanto@uide.edu.ec>
2026-03-25 13:18:27 +0000

    commit arsenal2

--- filesystem.sh
+++ filesystem.sh
@@ -1,1 +1,3 @@
 pwd #print working directory
+ls -F #sirve para listar archivos y carpetas agregando un símbolo al final según el tipo de archivo
+ls -i #sirve para ver el identificador interno de cada archivo o directorio dentro del sistema de archivos
