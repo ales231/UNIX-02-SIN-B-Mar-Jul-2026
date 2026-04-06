f0e887c5df294a39a0682a90bd8405c204943d75
ales231 <alalbanto@uide.edu.ec>
2026-04-06 12:34:26 +0000

    commit_cd

---
+++ script_boot.sh
@@ -0,0 +1,10 @@
+#Changes your current directory to your home directory by default.
+cd
+# Takes you to the root directory of the whole Linux file system.
+cd /
+#Moves you directly to the folder named codespace inside /home.
+cd /home/codespace
+#Takes you to your home directory, because ~ is a shortcut for home.
+cd ~
+#Also takes you to your home directory, because $HOME is the environment variable that stores the path of your home folder.
+cd $HOME 
