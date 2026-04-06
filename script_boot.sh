73a0786d88248203468140630f903615a69b90f4
ales231 <alalbanto@uide.edu.ec>
2026-04-06 12:41:26 +0000

    commit_ls -lai

--- script_boot.sh
+++ script_boot.sh
@@ -8,3 +8,7 @@ cd /home/codespace
 cd ~
 #Also takes you to your home directory, because $HOME is the environment variable that stores the path of your home folder.
 cd $HOME 
+#ls -lai shows the inode, permissions, links, owner, group, size, date, and name of . (the current directory) and .. (the parent directory).
+total 12
+925545 drwxr-xr-x 2 codespace codespace 4096 Apr  6 12:33 .
+918515 drwxr-x--- 1 codespace codespace 4096 Apr  6 12:33 ..
