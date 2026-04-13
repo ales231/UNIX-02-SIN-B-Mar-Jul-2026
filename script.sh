f100e541be1a27a0b61df9bb51d8dc18a78b6789
ales231 <alalbanto@uide.edu.ec>
2026-04-13 12:59:25 +0000

    man

--- script.sh
+++ script.sh
@@ -7,3 +7,5 @@ ls -lah #All three do the same thing, but it's a way to shorten the command and 
 mkdir -- -rf #Creates a directory literally named -rf by stopping option parsing with --.
 rm -- -rf #Removes a file literally named -rf by treating it as a filename, not as options.
 rmdir -- -rf #Removes an empty directory literally named -rf by treating it as a name, not as flags.
+ls --help  #Give me an explanation of the ls 
+man ls #This a manual for ls
