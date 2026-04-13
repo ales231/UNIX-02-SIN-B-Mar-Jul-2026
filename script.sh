3f0d6f59d47c7d57c1ce590a9f97e9b820e571e3
ales231 <alalbanto@uide.edu.ec>
2026-04-13 12:46:18 +0000

    ls -lah

--- script.sh
+++ script.sh
@@ -3,4 +3,4 @@ ls --all #is the same
 ls -a / #is used to list all files and directories in the root directory of the system (/), including hidden ones.
 ls -l -a -h
 ls -l -ah
-ls -lah #All three do the same thing, but it's a way to shorten the command.
+ls -lah #All three do the same thing, but it's a way to shorten the command and list all files.
