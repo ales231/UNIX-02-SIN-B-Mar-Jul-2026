34d9aecd01352eff06db3ecf286f1bf9a7a59166
ales231 <alalbanto@uide.edu.ec>
2026-04-15 13:10:34 +0000

    exercise5

--- ejercicio1.sh
+++ ejercicio1.sh
@@ -2,3 +2,7 @@ echo '#!/bin/sh' > hola.sh #Create a file called hello.sh and print the message 
 echo 'echo "Hola desde mi primer script"' >> hola.sh #appends the line echo "Hola desde mi primer script" to the file hola.sh.
 cat hola.sh #shows what the file contains
 ./hola.sh #permission denied
+ls -l hola.sh
+chmod +x hola.sh
+ls -l hola.sh
+./hola.sh #This block first shows the file permissions of hola.sh, then gives it execute permission, shows the updated permissions, and finally runs the script.
