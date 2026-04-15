77eca85d515a29539b05c97b5dc9e043c867dedf
ales231 <alalbanto@uide.edu.ec>
2026-04-15 13:38:27 +0000

    exercise2.1

---
+++ ejercicio2.sh
@@ -0,0 +1,5 @@
+touch prueba.txt #Creates the file prueba.txt if it does not exist, or updates its modification timestamp if it already exists.
+chmod 600 prueba.txt #Changes the permissions so that only the owner can read and write the file.
+ ls -l prueba.txt #Displays the file details, including its current permissions.
+ chmod 755 prueba.txt #Changes the permissions so that the owner can read, write, and execute the file, while others can only read and execute it.
+ ls -l prueba.txt #Displays the file details again to verify the permission changes.
