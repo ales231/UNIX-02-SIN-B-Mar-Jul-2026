f71b4ab9376c696b509c2db1a7148480fa5d5903
ales231 <alalbanto@uide.edu.ec>
2026-04-15 13:07:12 +0000

    exercise4

--- ejercicio1.sh
+++ ejercicio1.sh
@@ -1,3 +1,4 @@
 echo '#!/bin/sh' > hola.sh #Create a file called hello.sh and print the message in quotation marks
 echo 'echo "Hola desde mi primer script"' >> hola.sh #appends the line echo "Hola desde mi primer script" to the file hola.sh.
 cat hola.sh #shows what the file contains
+./hola.sh #permission denied
