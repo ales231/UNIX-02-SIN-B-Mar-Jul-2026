69cc2a405024e54d7aa378ebb3f8a1335d6d7650
ales231 <alalbanto@uide.edu.ec>
2026-04-15 13:04:13 +0000

    exercise2

--- ejercicio1.sh
+++ ejercicio1.sh
@@ -1,1 +1,2 @@
 echo '#!/bin/sh' > hola.sh #Create a file called hello.sh and print the message in quotation marks
+echo 'echo "Hola desde mi primer script"' >> hola.sh #appends the line echo "Hola desde mi primer script" to the file hola.sh.
