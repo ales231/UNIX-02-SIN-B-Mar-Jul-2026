3840146cf7758ca3a2e649ca671b8e1839637583
ales231 <alalbanto@uide.edu.ec>
2026-04-27 12:54:03 +0000

    sudo

--- script4.sh
+++ script4.sh
@@ -6,3 +6,7 @@ umask 027 #Changes the default permission mask for new files and directories.
 touch archivo2 #Creates an empty file called archivo2, but now using the new umask 027, so it will probably have permissions like rw-r-----.
 mkdir directorio2 #Creates a new directory called directorio2, also using umask 027, so it will probably have permissions like rwxr-x---.
 ls -l #Shows the files and directories in long format, including permissions, owner, group, size, and date.
+sudo apt-get update #Updates the local package list from the repositories. It does not install anything; it only refreshes the information about available packages and versions.
+sudo apt-get install acl #This package provides commands like setfacl and getfacl, which are used to manage Access Control Lists in Linux.
+sudo chown R$(whoami) . #Changes the owner of the current directory and everything inside it to your current user.
+sudo setfacl -bnR #Removes extra ACL permissions from the current directory and everything inside it.
