574738247fa0b9b2b92b123fc8844f95b2984317
ales231 <alalbanto@uide.edu.ec>
2026-05-04 12:47:15 +0000

    id

---
+++ security.sh
@@ -0,0 +1,2 @@
+cat /etc/passwd | head -10 ## Shows the first 10 lines of the /etc/passwd file directly.
+id # Shows the identity information of the current user, such as user ID, group ID, and group memberships.
