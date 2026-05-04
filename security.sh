873023857f1f29f19f0e0205f1e78c2e9844745b
ales231 <alalbanto@uide.edu.ec>
2026-05-04 13:28:39 +0000

    security

--- security.sh
+++ security.sh
@@ -1,2 +1,4 @@
 cat /etc/passwd | head -10 ## Shows the first 10 lines of the /etc/passwd file directly.
 id # Shows the identity information of the current user, such as user ID, group ID, and group memberships.
+groups # Shows the groups that the current logged-in user belongs to.
+groups $USER # Shows the groups that the user stored in the $USER variable belongs to. 
