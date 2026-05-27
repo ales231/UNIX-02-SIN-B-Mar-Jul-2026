16def4298647be7c38349defd7aff43cd2a4946d
ales231 <alalbanto@uide.edu.ec>
2026-05-27 13:26:23 +0000

    2

--- security2.sh
+++ security2.sh
@@ -3,6 +3,17 @@
 id
 id-gn # only the primary group name
 # Create a file and see which group inherits 
-touch ~/test_inherited_group.txt
-Is -la ~/test_inherited_group.txt
+touch ~/test_grupo_heredado.txt
+Is -la ~/test_grupo_heredado.txt
 # The group is the user's primary group
+# View the current group
+id -gn
+echo "Current group: $(id -gn)"
+# Create a file before newgrp
+touch ~/antes_de_newgrp.txt
+is -la ~/antes_de_newgrp.txt
+# Change to the developers group
+newgrp desarrolladores
+# Verify that the active group has changed
+id -gn
+echo "New active group: $(id-gn)"
