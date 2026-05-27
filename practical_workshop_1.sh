4fae0421d6d7bab37a87668f45d00f561fb36984
ales231 <alalbanto@uide.edu.ec>
2026-05-27 19:50:36 +0000

    delete by rm *

--- practical_workshop_1.sh
+++
@@ -1,8 +0,0 @@
-uname -a
-which gpg
-gpg --version
-gpg --full-generate-key
-gpg --list-keys
-gpg --armor --export
-gpg --armor --export alexfalbant@gmail.com> mi_llave_publica.asc
-cat mi_llave_publica.asc
--- script_gnupg.sh
+++
@@ -1,7 +0,0 @@
-gpg --list-secret-keys -keyid-format=long  #para listar las llaves
-gpg --armor --export-secret-keys 2BD74A3FCDD98B69 #con nuestra firma ejecutamos este comando y exporta tu llave privada
-gpg --list-keys #antes de hacer publica vamos a listar las llaves
-gpg --import MICOMPA_llave_publica.asc #con esto exportamos la llave de my friend
-gpg --output doc_no_cifrado.txt --encrypt --recipient 573E1A32AF37CBFA47B03919B84A19B106C03AD7 doc_no_cifrado.txt #usamos el hash de la clave publica que importamos de nuestro compañero para cifrar el documento
-gpg --decrypt MICOMPA_doc_cifrado.txt #lo traducimos a nuestro idioma el mensaje que nos mando nuesteo companero
-gpg --output doc_no_cifrado_firmado.txt --clearsign doc_no_cifrado.txt #Con este comando firmamos en texto limpio nuestro documento
