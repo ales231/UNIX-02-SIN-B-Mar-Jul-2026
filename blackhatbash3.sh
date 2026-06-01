950e0e72b7682c05d5d311a62bbd93312a78eb2b
ales231 <alalbanto@uide.edu.ec>
2026-06-01 13:43:00 +0000

    unset book

--- blackhatbash3.sh
+++ blackhatbash3.sh
@@ -3,3 +3,9 @@ book="black hat bash"
 echo "This book's name is ${book}"
 
 echo "This book's nmae is $book" 
+
+root_directory=$(ls -ld /)
+echo "${root_directory}"
+book="Black hat bash"
+unset book
+echo "${book}"
