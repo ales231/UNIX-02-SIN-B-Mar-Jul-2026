a21a9ab685c662ece02b505360301c110d03acad
ales231 <alalbanto@uide.edu.ec>
2026-06-01 13:54:44 +0000

    loca

--- local_scope_variable.sh
+++ local_scope_variable.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-PUBLISHER="No Starch Press"
+PUBLISHER="No Starch Press"  #global variable works inside the entire code
  print_name(){ 
-    local name name="Black Hat Bash" echo "${name} by ${PUBLISHER}" 
+    local name  #local variable only works inside the function
+    name="Black Hat Bash" echo "${name} by ${PUBLISHER}" 
     } 
 
 print_name 
 echo "Variable ${name} will not be printed because it is a local variable."
+
