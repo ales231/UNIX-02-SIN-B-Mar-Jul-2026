08ada5af50d4b3fd50be9a2d05a30d430b500b99
ales231 <alalbanto@uide.edu.ec>
2026-06-03 13:32:33 +0000

    redirecciones

--- blackhatbash5.sh
+++ blackhatbash5.sh
@@ -3,3 +3,10 @@ touch test && touch test123
 (ls; ps)
 ls; ps; whoami
 lzl || echo "el comando lzl fallo"
+ls -l / &> stdout_and_stderr.txt
+cat stdout_and_stderr.txt
+ls -l / 1> stdout.txt 2> stderr.txt
+cat stdout.txt
+cat stderr.txt
+lzl 2> error.txt 
+cat error.txt
