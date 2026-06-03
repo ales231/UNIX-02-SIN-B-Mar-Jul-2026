57f5f9857bf598fd9ff0dea44be15ca03624b8d1
ales231 <alalbanto@uide.edu.ec>
2026-06-03 13:07:56 +0000

    rm

--- background.sh
+++ background.sh
@@ -4,4 +4,5 @@ echo "Sleeping for 10 seconds..." ❶ sleep 10 &
 # Creates a file echo "Creating the file test123" 
 touch test123 
 # Deletes a file 
-echo "Deleting the file test123" rm test123
+echo "Deleting the file test123" 
+rm test123
