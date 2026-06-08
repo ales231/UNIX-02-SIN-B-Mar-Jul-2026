ba34beaddb58531ee8c7b356a98ea33d35a60532
ales231 <alalbanto@uide.edu.ec>
2026-06-08 12:57:01 +0000

    argumentos

--- blackhatbash6.sh
+++ blackhatbash6.sh
@@ -5,3 +5,7 @@ TARGET="${1}"
 echo "Running the script ${SCRIPT_NAME}..."
 echo "Pinging the target: ${TARGET}..."
 ping "${TARGET}"
+echo "The arguments are: $@"
+echo "The total number of arguments are: $#"
+for args in "$@"; do 
+echo "${args}"
