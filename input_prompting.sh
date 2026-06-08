6bbf5168b44ac006e30f1e019fda0252d06752be
ales231 <alalbanto@uide.edu.ec>
2026-06-08 13:22:56 +0000

    exit

--- input_prompting.sh
+++ input_prompting.sh
@@ -8,3 +8,5 @@ ls -l > /dev/null
 echo "The status code of the ls command was: $?"
 lzl 2> /dev/null
 echo "The status code of the non-existing lzl command was: $?"
+echo "Exiting with status code: 223"
+exit 223
