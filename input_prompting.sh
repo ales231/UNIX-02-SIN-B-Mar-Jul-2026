26f61d38e520220fa1fdece343717ddd3e0aa348
ales231 <alalbanto@uide.edu.ec>
2026-06-08 13:15:18 +0000

    experiment

--- input_prompting.sh
+++ input_prompting.sh
@@ -4,3 +4,7 @@ read -r firstname
 echo "What is your last name?"
 read -r lastname
 echo "Your first name is ${firstname} and your last name is ${lastname}"
+ls -l > /dev/null
+echo "The status code of the ls command was: $?"
+lzl 2> /dev/null
+echo "The status code of the non-existing lzl command was: $?"
