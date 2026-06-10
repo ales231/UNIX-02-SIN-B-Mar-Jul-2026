01d3fb1b7d3eb7a683d33ef6f252c75ef84e249b
ales231 <alalbanto@uide.edu.ec>
2026-06-10 12:30:48 +0000

    nostarch

--- test_if_file_exist.sh
+++ test_if_file_exist.sh
@@ -6,3 +6,10 @@ exit 1
 else
 touch "${FILENAME}"
 fi
+VARIABLE_ONE="nostarch"
+VARIABLE_TWO="nostarch"
+if [[ "${VARIABLE_ONE}" == "${VARIABLE_TWO}" ]]; then
+echo "They are equal!"
+else
+echo "They are not equal!"
+fi
