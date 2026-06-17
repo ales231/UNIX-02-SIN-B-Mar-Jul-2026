aadcaa8f9018fd0520adddc1fd9b12fff676d93e
ales231 <alalbanto@uide.edu.ec>
2026-06-17 13:15:24 +0000

    Job Control and sleep

--- blackhatbash8.sh
+++ blackhatbash8.sh
@@ -11,3 +11,13 @@ sed '1d' newlog1.txt
 sed '$d' newlog1.txt
 sed '$d' newlog1.txt > newlogL.txt
 sed -n '2,15 p' log.txt
+sleep 100 &
+#[1] 19774   Background job #1 started with PID 19774
+
+ps -ef | grep sleep
+root           1       0  0 12:20 ?        00:00:00 /bin/sh -c echo Container started trap "exit 0" 15  exec "$@" while sleep 1 & wait $!; do :; done -
+root       19774    1171  0 13:11 pts/0    00:00:00 sleep 100
+root       20100       1  0 13:12 ?        00:00:00 sleep 1
+root       20123    1171  0 13:12 pts/0    00:00:00 grep sleep   #Shows all processes containing "sleep": the container keepalive (sleep 1), your job (sleep 100), and the grep itself
+jobs 
+[1]+  Ejecutando                 sleep 100 &  #Job #1 is still running in the background
