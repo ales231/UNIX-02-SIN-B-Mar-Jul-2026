71410dfc376d3509039ff7cbd867d1a0f1b75683
ales231 <alalbanto@uide.edu.ec>
2026-06-17 12:56:34 +0000

    Editing Streams with sed

--- blackhatbash8.sh
+++ blackhatbash8.sh
@@ -4,3 +4,6 @@ awk '{print $1,$NF}' log.txt
 awk -F',' '{print $1}' example_csv.txt
 awk 'NR < 10' log.txt
 grep "42.236.10.117" log.txt | awk '{print $7}'
+sed 's/Mozilla/Godzilla/g' log.txt
+grep "Mozilla" log.txt #Using grep with quotation marks, filter for the word "mozilla" I also verify that it was replaced
+
